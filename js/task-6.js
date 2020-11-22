b91844a2063c64abfa1d860c258d3b346e878185
Matviienko-Artem <68031006+Matviienko-Artem@users.noreply.github.com>
2020-11-22 23:24:21 +0200

    переделал 6 задачу

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,13 @@
 const inputRef = document.querySelector("#validation-input");
 
 function colorLength() {
-  inputRef.value.length >= inputRef.dataset.length
-    ? inputRef.classList.add("valid") || inputRef.classList.remove("invalid")
-    : inputRef.classList.add("invalid") || inputRef.classList.remove("valid");
+  if (inputRef.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  }
 }
 
 inputRef.addEventListener("blur", colorLength);
--- js/task-8.js
+++ js/task-8.js
@@ -1,97 +1,18 @@
-const users = [
-  {
-    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    id: "249b6175-5c30-44c6-b154-f120923736f5",
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    id: "e1bf46ab-7168-491e-925e-f01e21394812",
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
+{
+  /* <div id="controls">
+  <input type="number" min="0" max="100" step="1" />
+  <button type="button" data-action="render">Создать</button>
+  <button type="button" data-action="destroy">Очистить</button>
+</div>
 
-const getUsersWithFriend = (array, friendName) =>
-  array
-    .filter(({ friends }) => friends.includes(friendName))
-    .map(({ name }) => name);
+<div id="boxes"></div> */
+}
 
-console.table(getUsersWithFriend(users, "Briana Decker"));
-// [ 'Sharlene Bush', 'Sheree Anthony' ]
+const inputNambersRef = document.querySelector("#controls>input");
+const btnCreateRef = document.querySelector('button[data-action="render"]');
+const btnDestroyRef = document.querySelector('button[data-action="destroy"]');
+const boxesForDivRef = document.querySelector("#boxes");
+console.log(inputNambersRef);
 
-console.table(getUsersWithFriend(users, "Goldie Gentry"));
-// [ 'Elma Head', 'Sheree Anthony' ]
+const createElement = document.createElement("div");
+boxesForDivRef.append(createElement);
